6f3a50c648cc21e13584e1f2056ee02d727a8986
nuhgooyin <dannguyenbox@gmail.com>
2025-01-11 16:00:37 -0500

    added help screen + interactive vs. terminal mode + fixed bug

--- sync_to_ssds.py
+++ sync_to_ssds.py
@@ -10,6 +10,7 @@ from watchdog.events import FileSystemEventHandler
 import pyudev
 import hashlib
 import sys
+import inquirer
 
 class SyncManager:
     def __init__(self, target_dir, mirror_drives):
@@ -35,20 +36,24 @@ class SyncManager:
 
     def initial_sync(self):
         for drive in self.mirror_drives:
-            if self.is_drive_mounted(drive):
-                self.mounted_drives.add(drive)
-                self.sync_directory(drive)
+            if isinstance(drive, dict):
+                drive_path = drive['value']
             else:
-                print(f"Drive {drive} is not mounted. Skipping.")
+                drive_path = drive
+            if self.is_drive_mounted(drive_path):
+                self.mounted_drives.add(drive_path)
+                self.sync_directory(drive_path)
+            else:
+                print(f"Drive {drive_path} is not mounted. Skipping.")
 
     def is_drive_mounted(self, drive):
         partitions = psutil.disk_partitions(all=True)
-        print(partitions)
         for partition in partitions:
             if os.path.realpath(drive) == os.path.realpath(partition.mountpoint):
                 return True
         return False
 
+# TODO: CHECK MAX SIZE ISSUES
     def sync_directory(self, drive):
         destination = Path(drive) / self.target_dir.name
         if not destination.exists():
@@ -121,18 +126,31 @@ class SyncManager:
         observer.start()
         observer.join()
 
-    def device_event(self, action, device):
-        drive_path = device.device_node
-        if device.device_node in self.mirror_drives:
-            if action == 'add':
-                print(f"Drive {drive_path} mounted.")
-                with self.lock:
-                    self.mounted_drives.add(drive_path)
-                    self.sync_directory(drive_path)
-            elif action == 'remove':
-                print(f"Drive {drive_path} unmounted.")
-                with self.lock:
-                    self.mounted_drives.discard(drive_path)
+    def device_event(self, device):
+        if not device.device_node:
+            return
+            
+        # Find the mount point for this device
+        partitions = psutil.disk_partitions(all=True)
+        for partition in partitions:
+            if partition.device == device.device_node:
+                mount_point = partition.mountpoint
+                if mount_point in self.mirror_drives:
+                    action = device.action
+                    if action == 'add' or action == 'change':
+                        print(f"Drive {mount_point} mounted.")
+                        print(f"Syncing drive {mount_point} ...")
+                        with self.lock:
+                            self.mounted_drives.add(mount_point)
+                            self.sync_directory(mount_point)
+                    elif action == 'remove':
+                        print(f"Drive {mount_point} unmounted.")
+                        with self.lock:
+                            self.mounted_drives.discard(mount_point)
+                    elif action == 'move':
+                        print(f"Drive {mount_point} renamed/moved.")
+                    else:
+                        print("Error: Unhandled device event.")
 
     def update_changes(self, src_path):
         with self.lock:
@@ -250,15 +268,142 @@ class DirectoryEventHandler(FileSystemEventHandler):
         print(f"{'Directory' if event.is_directory else 'File'} deleted: {event.src_path}")
         self.sync_manager.update_changes(event.src_path)
 
+def get_available_drives():
+    """Get list of mounted drives excluding root partition."""
+    drives = []
+    partitions = psutil.disk_partitions(all=True)
+    for partition in partitions:
+        # Skip root partition and non-writable locations
+        if partition.mountpoint != '/' and not partition.mountpoint.startswith('/boot'):
+            drives.append({
+                'name': f"{partition.mountpoint} ({partition.device})",
+                'value': partition.mountpoint
+            })
+    return drives
+
+def get_available_directories(current_path='/home'):
+    """Get list of directories in the specified path with navigation options."""
+    directories = []
+    
+    # Add parent directory option if not at root
+    if current_path != '/':
+        directories.append({
+            'name': '../ (Go up one level)',
+            'value': str(Path(current_path).parent)
+        })
+    
+    try:
+        # Add all directories in current path
+        for entry in os.scandir(current_path):
+            if entry.is_dir() and not entry.name.startswith('.'):
+                # Add trailing slash to indicate it's a directory that can be entered
+                directories.append({
+                    'name': f'{entry.name}/',
+                    'value': entry.path
+                })
+    except PermissionError:
+        pass
+    
+    # Add option to select current directory
+    directories.append({
+        'name': f'[SELECT] Current Directory: {current_path}',
+        'value': f'SELECT:{current_path}'
+    })
+    
+    return directories
+
+def interactive_selection():
+    """Prompt user to select target directory and mirror drives."""
+    current_path = '/home'
+    path_history = [current_path]  # Add this line to store history
+    
+    while True:
+        questions = [
+            inquirer.List(
+                'target_directory',
+                message='Navigate and select the target directory (select [SELECT] option to choose current directory)',
+                choices=get_available_directories(current_path),
+                # Add these parameters to enable history navigation
+                carousel=True,
+                default=path_history[-1]
+            ),
+        ]
+        
+        answer = inquirer.prompt(questions)
+        if not answer:
+            sys.exit(1)
+            
+        selected = answer['target_directory']
+        
+        # Check if user selected current directory
+        if selected['value'].startswith('SELECT:'):
+            target_dir = selected['value'].split(':', 1)[1]
+            break
+        else:
+            current_path = selected['value']
+            path_history.append(current_path)  # Add new path to history
+    
+    # After selecting target directory, prompt for mirror drives
+    questions = [
+        inquirer.Checkbox(
+            'mirror_drives',
+            message='Select the mirror drives (space to select, enter to confirm, up/down to navigate)',
+            choices=get_available_drives(),
+            carousel=True,  # Enable wrapping around the list
+            default=[]  # Start with no drives selected
+        ),
+    ]
+    
+    answers = inquirer.prompt(questions)
+    
+    if not answers or not answers['mirror_drives']:
+        print("Error: You must select at least one mirror drive.")
+        sys.exit(1)
+
+    # clean mirror drive input
+    cleaned_ans = []
+    for answer in answers['mirror_drives']:
+        cleaned_ans.append(answer['value'])
+
+    return target_dir, cleaned_ans
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Mirror a directory to multiple drives.")
-    parser.add_argument("target_directory", help="Path to the target directory to monitor.")
-    parser.add_argument("mirror_drives", nargs='+', help="List of mirror drive mount points.")
-    return parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="Mirror a directory to multiple drives.",
+        epilog="""
+Examples:
+  # Interactive mode (recommended):
+  python %(prog)s -i
+  
+  # Terminal mode:
+  python %(prog)s --target-directory /path/to/dir --mirror-drives /mnt/drive1 /mnt/drive2
+        """,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("--interactive", "-i", action="store_true", 
+                       help="Use interactive mode to select directories and drives")
+    parser.add_argument("--target-directory", 
+                       help="Path to the target directory to monitor")
+    parser.add_argument("--mirror-drives", nargs='+', 
+                       help="List of mirror drive mount points")
+    
+    # If no arguments provided, print help and exit
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+        
+    args = parser.parse_args()
+    
+    if args.interactive:
+        return interactive_selection()
+    elif args.target_directory and args.mirror_drives:
+        return args.target_directory, args.mirror_drives
+    else:
+        return interactive_selection()
 
 def main():
     args = parse_arguments()
-    sync_manager = SyncManager(args.target_directory, args.mirror_drives)
+    sync_manager = SyncManager(args[0], args[1])
     sync_manager.initial_sync()
 
     drive_thread = threading.Thread(target=sync_manager.monitor_drives, daemon=True)
